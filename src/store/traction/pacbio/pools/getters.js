39e1d858188b92cd54ab184d026e3a70ecfb93aa
James Glover <james.glover@sanger.ac.uk>
2021-07-21 08:18:18 +0100

    Refactor pools getter

--- src/store/traction/pacbio/pools/getters.js
+++ src/store/traction/pacbio/pools/getters.js
@@ -1,21 +1,22 @@
+const serializeLibrary = (state) => ({ id, type, requests, tags }) => {
+  const { sample_name } = state.requests[requests[0]]
+  const { group_id } = state.tags[tags[0]]
+  return { id, type, sample_name, group_id }
+}
+
+const serializePool = (state) => ({ id, type, libraries: libraryIds, tubes: [tubeId] }) => {
+  const libraries = libraryIds.map((libraryId) =>
+    serializeLibrary(state)(state.libraries[libraryId]),
+  )
+  const { barcode } = state.tubes[tubeId]
+
+  return { id, type, libraries, barcode }
+}
+
 const getters = {
   poolRequest: (state, getters, rootState) => rootState.api.traction.pacbio.pools,
-  pools: (state) => {
-    return Object.values(state.pools).map((pool) => {
-      const libraries = pool.libraries.map((libraryId) => {
-        const { id, type, requests, tags } = state.libraries[libraryId]
-        const { sample_name } = state.requests[requests[0]]
-        const { group_id } = state.tags[tags[0]]
-        return { id, type, sample_name, group_id }
-      })
-      const { barcode } = state.tubes[pool.tubes[0]]
-
-      return { ...pool, libraries, barcode, tubes: undefined }
-    })
-  },
-  poolByBarcode: (state, { pools }) => (barcode) => {
-    return pools.find((pool) => pool.barcode === barcode)
-  },
+  pools: (state) => Object.values(state.pools).map(serializePool(state)),
+  poolByBarcode: (state, { pools }) => (barcode) => pools.find((pool) => pool.barcode === barcode),
 }
 
 export default getters
